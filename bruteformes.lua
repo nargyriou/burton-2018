4e9653ca90b3185021299c5391daf334a29cc73c
NULL <NULL>
2018-02-22 23:27:03 +0100

    woo

--- bruteformes.lua
+++ bruteformes.lua
@@ -2,18 +2,27 @@ local function printRect(width, height)
 	for i=1, height do
 		print(string.rep("#", width))
 	end
-	print(" ")
 end
 
 local function bruteforme(cellno)
+	local results = {}
+
 	for width = 1, cellno do
 		local maxHeight = math.floor(cellno/width)
 		local minHeight = 1
 
 		for height = minHeight, maxHeight do
-			printRect(width, height)
+			table.insert(results, {width, height} )
 		end
 	end
+
+	return results
 end
+
+-- local res = bruteforme(9)
+-- for i,v in pairs(res) do
+-- 	printRect(v[1], v[2])
+-- 	print("")
+-- end
 
 return bruteforme
