448306aeb52dc62e780581bb0b4cf8249388fcfa
Quentin Gliech <quentingliech@gmail.com>
2018-03-01 19:22:40 +0100

    Change the structure a bit

--- parser.lua
+++ parser.lua
@@ -1,43 +1,46 @@
 local function parser(fd)
-	local firstline = fd:read("*l")
-	local R,C,F,N,B,T = string.match(firstline, "([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)")
-	R,C,F,N,B,T = tonumber(R), tonumber(C), tonumber(F), tonumber(N), tonumber(B), tonumber(T)
-	
-	assert(type(R) == "number", "R is invalid") -- number of rows of the grid ( 1 ≤ R ≤ 10 000)
-	assert(type(C) == "number", "C is invalid") -- number of columns of the grid ( 1 ≤ C ≤ 10 000)
-	assert(type(F) == "number", "F is invalid") -- number of vehicles in the fleet ( 1 ≤ F ≤ 1 000)
-	assert(type(N) == "number", "N is invalid") -- number of rides ( 1 ≤ N ≤ 10 000)
-	assert(type(B) == "number", "B is invalid") -- per-ride bonus for starting the ride on time ( 1 ≤ B ≤ 10 000)
-	assert(type(T) == "number", "T is invalid") -- number of steps in the simulation ( 1 ≤ T ≤ 10^9 )
+  local firstline = fd:read("*l")
+  local R,C,F,N,B,T = string.match(firstline, "([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)")
+  R,C,F,N,B,T = tonumber(R), tonumber(C), tonumber(F), tonumber(N), tonumber(B), tonumber(T)
 
-	local data = {}
-	while true do
-		local row = {}
-		local txtline = fd:read("*l")
+  assert(type(R) == "number", "R is invalid") -- number of rows of the grid ( 1 ≤ R ≤ 10 000)
+  assert(type(C) == "number", "C is invalid") -- number of columns of the grid ( 1 ≤ C ≤ 10 000)
+  assert(type(F) == "number", "F is invalid") -- number of vehicles in the fleet ( 1 ≤ F ≤ 1 000)
+  assert(type(N) == "number", "N is invalid") -- number of rides ( 1 ≤ N ≤ 10 000)
+  assert(type(B) == "number", "B is invalid") -- per-ride bonus for starting the ride on time ( 1 ≤ B ≤ 10 000)
+  assert(type(T) == "number", "T is invalid") -- number of steps in the simulation ( 1 ≤ T ≤ 10^9 )
 
-		if not txtline then break end
+  local data = {}
+  while true do
+    local txtline = fd:read("*l")
 
-		for c in string.gmatch(txtline, " ?([0-9]+)") do
-			table.insert(row, tonumber(c))
-		end
-		table.insert(data, row)
-	end
+    if not txtline then break end
 
-	data.R = R
-	data.C = C
-	data.F = F
-	data.N = N
-	data.B = B
-	data.T = T
+    local row_start, col_start, row_finish, col_finish, start, finish = string.match(firstline, "([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)")
+    table.insert(data, {
+      start={
+        row=tonumber(row_start),
+        col=tonumber(col_start),
+        step=tonumber(start),
+      },
+      finish={
+        row=tonumber(row_finish),
+        col=tonumber(col_finish),
+        step=tonumber(finish),
+      }
+    })
+  end
 
-	return data
+  data.R = R
+  data.C = C
+  data.F = F
+  data.N = N
+  data.B = B
+  data.T = T
+
+  return data
 end
 
--- local fd = io.open("datasets/b_should_be_easy.in", "r")
--- assert(fd)
--- local t = parser(fd)
-
--- local tprint = require "tprint"
--- print(tprint(t, {inline=false}))
-
 return parser
+
+-- vim:set noet sts=2 sw=2 ts=2:
