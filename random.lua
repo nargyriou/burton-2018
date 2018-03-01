707c33559e812b6bf325b3abe56fd252de4361f1
nargyriou <nicolas.argyriou95@gmail.com>
2018-03-01 22:00:28 +0100

    sous racage

---
+++ random.lua
@@ -0,0 +1,49 @@
+local parse = require "parser"
+local tprint = require "tprint"
+local socket = require "socket"
+local utils = require "utils"
+
+local function bullshit(data)
+	local vehicles = {}
+	-- init les vehicules
+	for i=1, data.F do
+		vehicles[i] = {rides = {}}
+		vehicles[i].n = 0
+		vehicles[i].step = 0
+	end
+
+	-- pour tous les rides
+	for i=1, data.N do
+		-- vehicule au hasard
+		local tries = 0
+		while tries < 10000 do
+			local v = vehicles[math.random(1, data.F)]
+			local ride = data[i]
+
+			-- print(tprint(ride, {inline=false}))
+
+			if v.step + ride.cost < ride.deadline then
+				-- on lui ajoute le ride i
+				table.insert(v.rides, {ride, v.step})
+				v.n = v.n+1
+				v.step = v.step + ride.cost
+				print("Ride " .. i .. " done")
+				break
+			end
+			tries = tries + 1
+		end
+	end
+
+	return vehicles
+end
+
+local seed = math.floor(   (socket.gettime()*10000) % 5000 )
+math.randomseed(seed)
+local fd = io.open("datasets/b_should_be_easy.in", "r")
+local data = parse(fd)
+--print(tprint(data, {inline=false}))
+local t = bullshit(data)
+
+print(tprint(t, {inline=false}))
+
+return bullshit
