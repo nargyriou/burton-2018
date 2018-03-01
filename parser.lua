1835cb380a1407d3bd8b4874c032d6720e994e2b
TsT <tst2005@gmail.com>
2018-03-01 19:14:07 +0100

    parser comment

--- parser.lua
+++ parser.lua
@@ -3,12 +3,12 @@ local function parser(fd)
 	local R,C,F,N,B,T = string.match(firstline, "([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)")
 	R,C,F,N,B,T = tonumber(R), tonumber(C), tonumber(F), tonumber(N), tonumber(B), tonumber(T)
 	
-	assert(type(R) == "number", "R is invalid")
-	assert(type(C) == "number", "C is invalid")
-	assert(type(F) == "number", "F is invalid")
-	assert(type(N) == "number", "N is invalid")
-	assert(type(B) == "number", "B is invalid")
-	assert(type(T) == "number", "T is invalid")
+	assert(type(R) == "number", "R is invalid") -- number of rows of the grid ( 1 ≤ R ≤ 10 000)
+	assert(type(C) == "number", "C is invalid") -- number of columns of the grid ( 1 ≤ C ≤ 10 000)
+	assert(type(F) == "number", "F is invalid") -- number of vehicles in the fleet ( 1 ≤ F ≤ 1 000)
+	assert(type(N) == "number", "N is invalid") -- number of rides ( 1 ≤ N ≤ 10 000)
+	assert(type(B) == "number", "B is invalid") -- per-ride bonus for starting the ride on time ( 1 ≤ B ≤ 10 000)
+	assert(type(T) == "number", "T is invalid") -- number of steps in the simulation ( 1 ≤ T ≤ 10^9 )
 
 	local data = {}
 	while true do
