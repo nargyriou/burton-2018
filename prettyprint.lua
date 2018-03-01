13050c176411883ef7b241f3a217c0c91007ba40
nargyriou <nicolas.argyriou95@gmail.com>
2018-03-01 19:49:14 +0100

    b

---
+++ prettyprint.lua
@@ -0,0 +1,27 @@
+-- 
+-- 
+-- 
+-- ╬
+-- 
+-- 
+-- 
+
+local parse = require "parser"
+
+local function prettyprint(data)
+
+end
+
+local data = parse("datasets/a_example.in")
+prettyprint(data)
+
+return prettyprint
+
+-- 		─	━	│	┃	┄	┅	┆	┇	┈	┉	┊	┋	┌	┍	┎	┏
+-- 		┐	┑	┒	┓	└	┕	┖	┗	┘	┙	┚	┛	├	┝	┞	┟
+-- 		┠	┡	┢	┣	┤	┥	┦	┧	┨	┩	┪	┫	┬	┭	┮	┯
+-- 		┰	┱	┲	┳	┴	┵	┶	┷	┸	┹	┺	┻	┼	┽	┾	┿
+-- 		╀	╁	╂	╃	╄	╅	╆	╇	╈	╉	╊	╋	╌	╍	╎	╏
+-- 		═	║	╒	╓	╔	╕	╖	╗	╘	╙	╚	╛	╜	╝	╞	╟
+-- 		╠	╡	╢	╣	╤	╥	╦	╧	╨	╩	╪	╫	╬	╭	╮	╯
+-- 		╰	╱	╲	╳	╴	╵	╶	╷	╸	╹	╺	╻	╼	╽	╾	╿
